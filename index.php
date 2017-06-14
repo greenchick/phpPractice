96cceaa33e3353c4977a1052922513006f8d00c5
greenchick <fuji.satoru.2015@gmail.com>
2017-06-15 05:56:02 +0900

    ログイン機能の追加用ブランチ

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-  require('functions.php');
+require_once('functions.php');
+if(isset($_SERVER['HTTP_REFERER'])){
+  $previousLink = parse_url($_SERVER['HTTP_REFERER']);
+  if ($previousLink['path'] == '/new.php'){
+    unsetSession();
+  };
+};
+setToken();
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -8,8 +15,35 @@
   <title>Home</title>
 </head>
 <body>
-  welcome hello world
+  WELCOME!
   <div>
+    <?php if($_SESSION['login'] !== ''): ?>
+      <p><?php echo 'ようこそ'.$_SESSION['login'].'さん'; ?></p>
+      <p><?php echo $_SESSION['login'].'さんでは無い場合は下のボタンでログアウトして下さい。'?></p>
+      <form action="store.php" method="POST">
+        <input type="hidden" name="type" value="logout">
+        <input type="submit" value="ログアウト">
+      </form>
+      <?php else : ?>
+        <p>ユーザーの方はログインして下さい。</p>
+        <form action="store.php" method="post" name="userConf">
+          <input type="hidden" name="token" value="<?php echo h($_SESSION['token']); ?>">
+          <div>
+            <?php if(isset($_SESSION['error'])): ?>
+              <p> <?php echo $_SESSION['error'] ?>
+              </p>
+            <?php endif; ?>
+            <p class="iblock">USER NAME</p>
+            <input type="text" name="user_name">
+          </div>
+          <div>
+            <p class="iblock">PASSWORD</p>
+            <input type="password" name="pw">
+          </div>
+          <input type="hidden" name="type" value="login">
+          <input type="submit" value="ログイン" name="submit">
+        </form>
+      <?php endif; ?>
     <a href="new.php">
       <p>新規作成</p>
     </a>
